18ff35f559c18a6d9c09eb0b73351cc3ddaba386
Kim Young Hoo <zerohoo.kim@gmail.com>
2014-03-30 20:07:03 +0900

    add info.html

--- wikisite/wiki/urls.py
+++ wikisite/wiki/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic import TemplateView
 
 import views
 
 urlpatterns = patterns('',
+    url(r'^accounts/info/$', TemplateView.as_view(template_name='registration/info.html')),
     url(r'^accounts/register/$', views.WikiRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^(?P<path>.*)$', views.index, name='index'),
