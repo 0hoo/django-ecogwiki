a9e71ebdf430c289e6392830db8606705869ba56
Kim Young Hoo <zerohoo.kim@gmail.com>
2014-03-30 19:53:05 +0900

    update ruls

--- wikisite/wiki/urls.py
+++ wikisite/wiki/urls.py
@@ -3,10 +3,7 @@ from django.conf.urls import patterns, url, include
 import views
 
 urlpatterns = patterns('',
-    #url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name' : 'wiki/login.html', }),
-    #url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page' : '/'}),
     url(r'^accounts/register/$', views.WikiRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^(?P<path>.*)$', views.index, name='index'),
-    #url(r'^accounts/signup/$', views.signup),
 )
