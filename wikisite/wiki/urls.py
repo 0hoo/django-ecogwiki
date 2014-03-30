877569be2cb8d7d4164bda2072e97aa6bccd9cf7
Kim Young Hoo <zerohoo.kim@gmail.com>
2014-03-30 23:27:24 +0900

    add password reset

--- wikisite/wiki/urls.py
+++ wikisite/wiki/urls.py
@@ -4,6 +4,13 @@ from django.views.generic import TemplateView
 import views
 
 urlpatterns = patterns('',
+    url(r'^accounts/password/reset/$', 'django.contrib.auth.views.password_reset',
+        {'post_reset_redirect' : '/accounts/password/reset/done/'}, name='password_reset'),
+    (r'^accounts/password/reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'post_reset_redirect' : '/accounts/password/done/'}, name='password_reset_confirm'),
+    (r'^accounts/password/done/$', 'django.contrib.auth.views.password_reset_complete'),
     url(r'^accounts/info/$', TemplateView.as_view(template_name='registration/info.html')),
     url(r'^accounts/register/$', views.WikiRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
--- wikisite/wikisite/settings.py
+++ wikisite/wikisite/settings.py
@@ -94,3 +94,11 @@ CACHES = {
         'LOCATION': 'unique-snowflake'
     }
 }
+
+if DEBUG:
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = '@example.com'
+    EMAIL_HOST_PASSWORD = ''
+    EMAIL_USE_TLS = True
+    DEFAULT_FROM_EMAIL = '@example.com'
