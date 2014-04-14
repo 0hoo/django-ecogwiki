9d9928d6d9d56f117161dff474ed997f470da165
Kim Young Hoo <zerohoo.kim@gmail.com>
2014-04-14 17:48:07 +0900

    add CustomBlogTest

--- wikisite/wiki/tests/test_blog.py
+++ wikisite/wiki/tests/test_blog.py
@@ -61,3 +61,58 @@ class DefaultBlogUnpublishTest(WikiTestCase):
         newer, older = WikiPage.get_posts_of(None)
         self.assertEqual(u'Hello 3', older.newer_title)
         self.assertEqual(u'Hello 1', newer.older_title)
+
+
+class CustomBlogTest(WikiTestCase):
+    def setUp(self):
+        super(CustomBlogTest, self).setUp()
+
+    def test_publish(self):
+        page = self.update_page(u'.pub Posts\nHello')
+        self.assertIsNotNone(page.published_at)
+        self.assertEqual('Posts', page.published_to)
+        self.assertEqual(1, len(WikiPage.get_posts_of('Posts')))
+
+    def test_specify_page_to_published_page(self):
+        # .pub -> .pub BBS
+        self.update_page(u'.pub\nHello', u'Hello')
+        page = self.update_page(u'.pub BBS\nHello', u'Hello')
+
+        self.assertIsNotNone(page.published_at)
+        self.assertEqual('BBS', page.published_to)
+
+    def test_change_page_of_published_page(self):
+        # .pub BBS1 -> .pub BBS2
+        self.update_page(u'.pub BBS1\nHello', u'Hello')
+        page = self.update_page(u'.pub BBS2\nHello', u'Hello')
+        self.assertIsNotNone(page.published_at)
+        self.assertEqual('BBS2', page.published_to)
+
+    def test_remove_page_of_published_page(self):
+        # .pub BBS -> .pub
+        self.update_page(u'.pub BBS\nHello', u'Hello')
+        page = self.update_page(u'.pub\nHello', u'Hello')
+        self.assertIsNotNone(page.published_at)
+        self.assertEqual(None, page.published_to)
+
+    def test_unpublish_published_page(self):
+        # .pub BBS -> (null)
+        self.update_page(u'.pub BBS\nHello', u'Hello')
+        page = self.update_page(u'Hello', u'Hello')
+        self.assertIsNone(page.published_at)
+        self.assertEqual(None, page.published_to)
+
+    def test_multiple_custom_blogs(self):
+        self.update_page(u'.pub B1', u'b1p1')
+        self.update_page(u'.pub B1', u'b1p2')
+        self.update_page(u'.pub B2', u'b2p1')
+        self.update_page(u'.pub B2', u'b2p2')
+
+        b1p1 = WikiPage.get_by_title(u'b1p1')
+        b1p2 = WikiPage.get_by_title(u'b1p2')
+        b2p1 = WikiPage.get_by_title(u'b2p1')
+        b2p2 = WikiPage.get_by_title(u'b2p2')
+        self.assertEqual('b1p2', b1p1.newer_title)
+        self.assertEqual('b1p1', b1p2.older_title)
+        self.assertEqual('b2p2', b2p1.newer_title)
+        self.assertEqual('b2p1', b2p2.older_title)
