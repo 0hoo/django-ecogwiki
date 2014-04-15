29ac678a53b51f6cc257b9bd3333685f75746f6d
Kim Young Hoo <zerohoo.kim@gmail.com>
2014-04-15 21:06:05 +0900

    add test_search

---
+++ wikisite/wiki/tests/test_search.py
@@ -0,0 +1,78 @@
+from .. import search
+from django.test import TestCase
+
+
+class ExpressionParserTest(TestCase):
+    def test_single_title(self):
+        expected = {
+            'pos': [u'Hello'],
+            'neg': [],
+        }
+        actual = search.parse_expression(u'+Hello')
+        self.assertEqual(expected, actual)
+
+    def test_multiple_titles(self):
+        expected = {
+            'pos': [u'What the', u'Is it'],
+            'neg': [u'Fun'],
+        }
+        actual = search.parse_expression(u'+What the -Fun +Is it')
+        self.assertEqual(expected, actual)
+
+    def test_no_space(self):
+        expected = {
+            'pos': [u'User-centered design'],
+            'neg': [],
+        }
+        actual = search.parse_expression(u'+User-centered design')
+        self.assertEqual(expected, actual)
+
+
+class EvaluationTest(TestCase):
+    def test_should_not_contain_self(self):
+        positives = {
+            u'A': {
+                u'A': 0.2,
+                u'B': 0.2,
+                u'C': 0.3,
+            },
+            u'B': {
+                u'B': 0.2,
+                u'C': 0.2,
+                u'D': 0.2,
+            }
+        }
+        negatives = {
+            u'C': {
+                u'A': 0.2,
+                u'E': 0.1,
+            }
+        }
+
+        expected = [u'D', u'E']
+        actual = search.evaluate(positives, negatives).keys()
+        self.assertEqual(expected, actual)
+
+    def test_complex_case(self):
+        positives = {
+            u'Page 1': {
+                u'A': 0.2,
+                u'B': 0.2,
+                u'C': 0.3,
+            },
+            u'Page 2': {
+                u'B': 0.2,
+                u'C': 0.2,
+                u'D': 0.2,
+            }
+        }
+        negatives = {
+            u'Page 3': {
+                u'A': 0.2,
+                u'E': 0.1,
+            }
+        }
+
+        expected = [u'C', u'B', u'D', u'A', u'E']
+        actual = search.evaluate(positives, negatives).keys()
+        self.assertEqual(expected, actual)
